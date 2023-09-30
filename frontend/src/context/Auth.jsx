beb581be65636120bb6bf3507ba2b46debdf0a32
gops21 <12116079@nitkkr.ac.in>
2023-09-30 12:52:28 +0530

    added apis and env file

--- frontend/src/context/Auth.jsx
+++ frontend/src/context/Auth.jsx
@@ -1,24 +1,26 @@
-import { createContext, useEffect, useState } from "react";
-import axios from "axios";
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
 
 const AuthContext = createContext({});
 
-export const AuthProvider = ({ children }) => {
+export function AuthProvider({ children }) {
   const [auth, setAuth] = useState(undefined);
 
   const verifyAuth = async () => {
-      const isLoggedIn = await axios.get('/api/auth/is_logged_in');
-      setAuth(isLoggedIn.data);      
-      return isLoggedIn.data;
-  }
+    const isLoggedIn = await axios.get(`/api/auth/is_logged_in`);
+    setAuth(isLoggedIn.data);
+    return isLoggedIn.data;
+  };
 
   useEffect(() => {
     verifyAuth();
   }, []);
 
-  return <AuthContext.Provider value={{auth, verifyAuth}}>
-  {children}
-  </AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ auth, verifyAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export default AuthContext;
