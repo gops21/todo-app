bc4c0b9327c920a677086b80455825f1ff295d04
gops21 <101035746+gops21@users.noreply.github.com>
2023-10-01 03:29:18 +0530

    Update getApiBaseUrl.js

--- frontend/src/utils/getApiBaseUrl.js
+++ frontend/src/utils/getApiBaseUrl.js
@@ -2,6 +2,6 @@ const getApiBaseUrl = () => {
   if (process.env.NODE_ENV === 'development') {
     return 'http://localhost:8000';
   }
-  return "https://todo-app-backend-tau.vercel.app/";
+  return "https://todo-app-backend-tau.vercel.app";
 };
 export default getApiBaseUrl;
